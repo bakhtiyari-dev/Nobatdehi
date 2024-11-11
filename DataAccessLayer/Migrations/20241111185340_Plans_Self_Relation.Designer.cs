b344a12cecb7f7fbdfd26d06dccd1fa652be69c5
amirhosein bakhtiyari <bakhtiyari086@gmail.com>
2024-11-12 02:48:20 +0330

    migr: plan self rrelation n-n for dependencies

--- DataAccessLayer/Migrations/20241111185340_Plans_Self_Relation.Designer.cs
+++ DataAccessLayer/Migrations/20241111185340_Plans_Self_Relation.Designer.cs
@@ -4,6 +4,7 @@ using DataAccessLayer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DataAccessLayer.Migrations
 {
     [DbContext(typeof(DatabaseContext))]
-    partial class DatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20241111185340_Plans_Self_Relation")]
+    partial class Plans_Self_Relation
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ DataAccessLayer/Migrations/20241111185340_Plans_Self_Relation.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccessLayer.Migrations
+{
+    /// <inheritdoc />
+    public partial class Plans_Self_Relation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Plans_Plans_PlanId",
+                schema: "Plan",
+                table: "Plans");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Plans_PlanId",
+                schema: "Plan",
+                table: "Plans");
+
+            migrationBuilder.DropColumn(
+                name: "PlanId",
+                schema: "Plan",
+                table: "Plans");
+
+            migrationBuilder.CreateTable(
+                name: "PlanDependencies",
+                schema: "Plan",
+                columns: table => new
+                {
+                    Dependencies = table.Column<int>(type: "int", nullable: false),
+                    PlanId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PlanDependencies", x => new { x.Dependencies, x.PlanId });
+                    table.ForeignKey(
+                        name: "FK_PlanDependencies_Plans_Dependencies",
+                        column: x => x.Dependencies,
+                        principalSchema: "Plan",
+                        principalTable: "Plans",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_PlanDependencies_Plans_PlanId",
+                        column: x => x.PlanId,
+                        principalSchema: "Plan",
+                        principalTable: "Plans",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PlanDependencies_PlanId",
+                schema: "Plan",
+                table: "PlanDependencies",
+                column: "PlanId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PlanDependencies",
+                schema: "Plan");
+
+            migrationBuilder.AddColumn<int>(
+                name: "PlanId",
+                schema: "Plan",
+                table: "Plans",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Plans_PlanId",
+                schema: "Plan",
+                table: "Plans",
+                column: "PlanId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Plans_Plans_PlanId",
+                schema: "Plan",
+                table: "Plans",
+                column: "PlanId",
+                principalSchema: "Plan",
+                principalTable: "Plans",
+                principalColumn: "Id");
+        }
+    }
+}
