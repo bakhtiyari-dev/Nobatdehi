7e6b006381ac10ba09344e6705d334676baa6ee4
bakhtiyari-dev <bakhtiyari086@gmail.com>
2024-11-15 21:36:36 +0330

    feat: pagination generic method feat: pagination dto

---
+++ BusinessLogicLayer/Application/ApplicationModels.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogicLayer.Application
+{
+    public class ApplicationModels
+    {
+
+    }
+
+    public class PaginatedResult<T>
+    {
+        public int TotalItems { get; set; }
+        public int PageNumber { get; set; }
+        public int PageSize { get; set; }
+        public int TotalPages { get; set; }
+        public IEnumerable<T> Items { get; set; }
+    }
+
+}
---
+++ BusinessLogicLayer/Application/IApplicationMethods.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogicLayer.Application
+{
+    public interface IApplicationMethods
+    {
+        public PaginatedResult<T> GetPaginatedResult<T>(IEnumerable<T> source, int pageNumber, int pageSize);
+    }
+}
